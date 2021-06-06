40836cd786cc607e83d795607d20771b12b612eb
KotaOgawa <k.ogawa.biz406@gmail.com>
2021-06-06 13:47:24 +0900

    :sparkles: タスク登録コンポーネントを作成

--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -7,7 +7,9 @@
           <router-link v-bind:to="{ name: 'task.list' }">
             <button class="btn btn-success">List</button>
           </router-link>
-          <button class="btn btn-success">ADD</button>
+          <router-link v-bind:to="{ name: 'task.create' }">
+            <button class="btn btn-success">ADD</button>
+          </router-link>
         </div>
       </nav>
     </div>
@@ -15,5 +17,5 @@
 </template>
 
 <script>
-  export default {}
+export default {}
 </script>
