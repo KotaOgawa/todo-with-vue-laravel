ccff522123527b707e97533efeee4425e646d41a
KotaOgawa <k.ogawa.biz406@gmail.com>
2021-06-09 17:06:34 +0900

    :sparkles: feat: タスク更新APIを作成

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('/tasks', 'TaskController@index');            // タスク一覧取得 index
 Route::post('/tasks', 'TaskController@store');           // タスク新規作成 store
 Route::get('/tasks/{task}', 'TaskController@show');      // タスク詳細取得 show
+Route::put('/tasks/{task}', 'TaskController@update');    // タスク更新    edit
